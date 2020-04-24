a2ed26823bdb5629c92236009fabae1f13168666
zhangzengmin <xiaomin0322@sina.com>
2020-04-24 16:28:21 +0800

    213

--- gateway-service/src/main/java/com/gittors/gateway/GateWayApplication.java
+++ gateway-service/src/main/java/com/gittors/gateway/GateWayApplication.java
@@ -1,8 +1,9 @@
 package com.gittors.gateway;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -14,9 +15,10 @@ import org.springframework.context.annotation.Bean;
 
 import com.gittors.gateway.config.MetricHandler;
 
-import io.netty.channel.ServerChannel;
+import io.netty.channel.EventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.internal.ThreadExecutorMap;
 import reactor.netty.http.server.HttpServer;
 
 /**
@@ -45,19 +47,26 @@ class EventLoopNettyCustomizer implements NettyServerCustomizer {
 	@Override
 	public HttpServer apply(HttpServer httpServer) {
 		ThreadPoolExecutor boss = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
-		//ThreadPoolExecutor work = (ThreadPoolExecutor) Executors.newFixedThreadPool(50);
-		//ThreadPoolExecutor work = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-		
-		ThreadPoolExecutor work =  new ThreadPoolExecutor(5, 5,
-                5L, TimeUnit.SECONDS,
-                //new LinkedBlockingQueue<Runnable>()
-                new ArrayBlockingQueue<>(5)
-				);
-		
-		//int nThreads, Executor executor；nThreads > executor.nThreads
-		
+		// ThreadPoolExecutor work = (ThreadPoolExecutor)
+		// Executors.newFixedThreadPool(50);
+		// ThreadPoolExecutor work = (ThreadPoolExecutor)
+		// Executors.newCachedThreadPool();
+
+		ThreadPoolExecutor work = new ThreadPoolExecutor(5, 5, 5L, TimeUnit.SECONDS,
+				// new LinkedBlockingQueue<Runnable>()
+				new ArrayBlockingQueue<>(5));
+
+		// int nThreads, Executor executor；nThreads > executor.nThreads
+
 		NioEventLoopGroup parentGroup = new NioEventLoopGroup(4, boss);
-		NioEventLoopGroup childGroup = new NioEventLoopGroup(10, work);
+		NioEventLoopGroup childGroup = new NioEventLoopGroup(10,work);
+
+		EventLoop eventLoop = childGroup.next();
+
+		final io.netty.util.concurrent.ThreadPerTaskExecutor workExecutor = (io.netty.util.concurrent.ThreadPerTaskExecutor)getExecutor(eventLoop);
+		
+		
+		
 		MetricHandler childHandler = new MetricHandler();
 
 		new Thread() {
@@ -73,13 +82,13 @@ class EventLoopNettyCustomizer implements NettyServerCustomizer {
 
 						System.out.println("boss当前活跃线程数:" + boss.getActiveCount());
 						System.out.println("work:当前活跃线程数" + work.getActiveCount());
-						
-						
+
 						System.out.println("boss最高线程数:" + boss.getLargestPoolSize());
 						System.out.println("work:最高线程数" + work.getLargestPoolSize());
 
 						System.out.println("连接数:" + childHandler.totalConnectionNumber.get());
-						
+
+
 						System.out.println("====================================================:");
 					} catch (InterruptedException e) {
 						e.printStackTrace();
@@ -90,5 +99,41 @@ class EventLoopNettyCustomizer implements NettyServerCustomizer {
 
 		return httpServer.tcpConfiguration(tcpServer -> tcpServer.bootstrap(serverBootstrap -> serverBootstrap
 				.group(parentGroup, childGroup).childHandler(childHandler).channel(NioServerSocketChannel.class)));
+	}
+
+	public static Object getExecutor(EventLoop eventLoop) {
+		Object executor = null;
+		try {
+			Field declaredField = getClassField(eventLoop.getClass(), "executor");
+			declaredField.setAccessible(true);
+			executor =  declaredField.get(eventLoop);
+			Field[] declaredFields = executor.getClass().getDeclaredFields();
+			for(Field f:declaredFields) {
+				System.out.println("f=="+f.getName());
+			}
+			declaredField = executor.getClass().getDeclaredField("val$executor");
+			declaredField.setAccessible(true);
+			executor = declaredField.get(executor);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return executor;
+	}
+
+	public static Field getClassField(Class<?> clazz, String fieldName) {
+		Field declaredField = null;
+		try {
+			declaredField = clazz.getDeclaredField(fieldName);
+		} catch (Exception e) {
+		}
+		if (declaredField != null) {
+			declaredField.setAccessible(true);
+			System.out.println("calss===="+clazz.getName());
+			return declaredField;
+		}
+		if (clazz.getSuperclass() != null) {
+			return getClassField(clazz.getSuperclass(), fieldName);
+		}
+		return null;
 	}
 }
